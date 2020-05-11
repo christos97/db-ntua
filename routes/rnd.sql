b79905f0c13411d474b0012542e38f821e4a8490
christos97 <sirmchristos@gmail.com>
2020-05-11 03:28:52 +0300

    more modal forms

--- routes/rnd.sql
+++
@@ -1,3 +0,0 @@
-UPDATE Stores as s, StoreAddress as sa 
-SET s.Operating_hours='09:00-21:00', s.Size_=450, sa.Street='Kappa', sa.Number_='25', sa.Postal_code='11472', sa.City='Kalamata' 
-WHERE Store_id=11;
